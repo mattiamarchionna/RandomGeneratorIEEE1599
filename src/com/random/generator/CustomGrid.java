e3f2092167b469caa5460b8c4dbfe322e05b55ee
Mattia Marchionna <mattiamarchionna1997@gmail.com>
2020-11-12 08:56:55 +0100

    aggiunta jComboBox

--- src/com/random/generator/CustomGrid.java
+++ src/com/random/generator/CustomGrid.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
+import java.util.Random;
 
 public class CustomGrid {
     public JPanel mainPanel;
@@ -15,9 +15,20 @@ public class CustomGrid {
     private JCheckBox soloNoteCheckBox;
     private JCheckBox soloPauseCheckBox;
     private JCheckBox entrambeCheckBox;
-    private JSpinner spinner1;
+    private JSpinner spinner1; private JSpinner spinner2; private JSpinner spinner3;
+    private JSpinner spinner4; private JSpinner spinner5; private JSpinner spinner6; private JSpinner spinner7;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private Random r = new Random();
 
     public CustomGrid() {
+
+        spinner1.setValue(r.nextInt(20)); spinner2.setValue(r.nextInt(20)); spinner3.setValue(r.nextInt(20));
+        spinner4.setValue(r.nextInt(20)); spinner5.setValue(r.nextInt(20)); spinner6.setValue(r.nextInt(20));
+        spinner7.setValue(r.nextInt(20));
+
+
+
         cartellaDiDestinazioneButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
